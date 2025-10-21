3fe8baa395db56bc79bd000a4dc95824fc63da44
TecXCTO <cto.tecx@gmail.com>
2025-10-21 16:38:39 +0530

    Update ready.sh to install PyTorch instead of TensorRT
    Commented out TensorRT installation commands and added PyTorch installation command.

--- ready.sh
+++ ready.sh
@@ -7,6 +7,9 @@ pip install jupyterlab numpy opencv-python
 #sudo apt install -y python3-pip python3-dev
 #pip install --upgrade pip setuptools wheel
 
-#!apt-get update -y
-#!apt-get install -y libnvinfer8 libnvinfer-dev libnvinfer-plugin8 libnvonnxparsers8 libnvparsers8
-#!apt-get install -y tensorrt
+#pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+
+#apt-get update -y
+#apt-get install -y libnvinfer8 libnvinfer-dev libnvinfer-plugin8 libnvonnxparsers8 libnvparsers8
+#apt-get install -y tensorrt
