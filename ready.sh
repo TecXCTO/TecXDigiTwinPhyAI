e0ab0098d0b93350a01f482dc7e4b4f2af5cd70e
TecXCTO <cto.tecx@gmail.com>
2025-10-21 16:19:51 +0530

    Add Python package installation commands in ready.sh
    Added installation commands for Python packages and commented out system package installations.

---
+++ ready.sh
@@ -0,0 +1,12 @@
+#sudo apt update
+#sudo apt install python3-pip
+pip install tensorpy
+pip install jupyterlab numpy opencv-python
+
+#sudo apt update
+#sudo apt install -y python3-pip python3-dev
+#pip install --upgrade pip setuptools wheel
+
+#!apt-get update -y
+#!apt-get install -y libnvinfer8 libnvinfer-dev libnvinfer-plugin8 libnvonnxparsers8 libnvparsers8
+#!apt-get install -y tensorrt
